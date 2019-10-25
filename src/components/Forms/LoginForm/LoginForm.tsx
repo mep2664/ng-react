7b611af80c07bed1f755006e41f3ccacf65f6464
Matt Pitcher <matt.e.pitcher@gmail.com>
2019-10-25 01:24:52 -0600

    remove graphql login mutation

--- src/components/Forms/LoginForm/LoginForm.tsx
+++ src/components/Forms/LoginForm/LoginForm.tsx
@@ -1,18 +1,7 @@
 import * as React from "react";
 import { TextInput } from "../..";
-import gql from "graphql-tag";
-import { useMutation } from "@apollo/react-hooks";
 import styled from "styled-components";
 import { emphasisType } from "../../../theme";
-
-export const LOGIN_USER = gql`
-    mutation LoginUser($email: String!, $password:String!) {
-        loginUser(email:$email, password:$password) {
-            token
-            error
-        }
-    }
-`;
 
 const FormHeader = styled.h2`
     text-align: center;
@@ -29,7 +18,6 @@ export const LoginForm: React.FC<ILoginFormProps> = ({ formId, emphasis = "Prima
     const [password, setPassword] = React.useState<string>("");
     const [error, setError] = React.useState<string>("");
     const [isSubmitting, setSubmitting] = React.useState<boolean>(false);
-    const [loginUser] = useMutation(LOGIN_USER);
 
     React.useEffect(() => {
         if (onSubmitChange) {
@@ -73,33 +61,6 @@ export const LoginForm: React.FC<ILoginFormProps> = ({ formId, emphasis = "Prima
 
             }, (reason) => { setError(reason.toString()); setSubmitting(false); });
         }, (reason) => { setError(reason.toString()); setSubmitting(false); });
-        // loginUser({
-        //     variables: data,
-        //     // TODO - figure out what to do with cache
-        //     update: (cache, response) => {
-        //         if (response.data.loginUser.token) {
-        //             setError(response.data.loginUser.error)
-        //             const d = new Date();
-        //             const numHours = 4;
-        //             const expires = d.setTime(d.getTime() + ((numHours * 60 * 60 * 1000)));
-        //             document.cookie = `uuid=${response.data.loginUser.token};expires=${expires};path=/`;
-        //             if (window.location.pathname === "/login" || window.location.pathname === "/home" || window.location.pathname === "/") {
-        //                 console.log("here");
-        //                 console.log(window.location.host + "/dashboard");
-        //                 window.location.assign(`${window.location.protocol}//${window.location.host}/dashboard`);
-        //             } else {
-        //                 window.location.reload()
-        //             }
-        //         } else {
-        //             if (response.data.loginUser.error) {
-        //                 setError(response.data.loginUser.error);
-        //             } else {
-        //                 setError("something went wrong...");
-        //             }
-        //         }
-        //         setSubmitting(false);
-        //     }
-        // }).catch((error) => { setError(error.message); setSubmitting(false) });
     }
 
     return (
